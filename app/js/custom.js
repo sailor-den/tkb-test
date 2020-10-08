f24fe2d21ca0f07ff3a195c123406e9021ce4237
Преображенский Денис <30519612+sailor-den@users.noreply.github.com>
2020-10-08 20:28:33 +0300

    Добавил параллакс

--- app/js/custom.js
+++ app/js/custom.js
@@ -2,6 +2,15 @@ document.addEventListener('DOMContentLoaded', function (){
 
   $(".content-page__arrows").mPageScroll2id();
 
+  // Параллакс
+  $(window).scroll(function(){
+    var st = $(this).scrollTop();
+
+    $(".content-page__title",).css({
+        "transform" : "translate(0%, " + st / 10 + "%"
+    });
+});
+
 
 });
 
